ab8d11fd6b42c4ded3dee583ae02ab96c162ac73
zahid sikdar <zahidsikdar786@gmail.com>
2022-10-29 22:15:06 +0530

    Added task-2

--- Task_2/Portfolio/script.js
+++ Task_2/Portfolio/script.js
@@ -5,5 +5,18 @@ $(document).ready(function(){
         }else {
             $('.navbar').removeClass("sticky");
         }
+    });
+
+    // toggle menu-navbar script 
+
+
+    $('.menu-btn').click(function(){
+        $('.navbar .menu').toggleClass("active");
+        $('.menu-btn i').toggleClass("active");
     })
+
+
+
+
+
 });
